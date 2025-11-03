962d699af55046585848557c4ea14bf37650ff50
Evaldas Dmitrišin <81704689+32Butterfly@users.noreply.github.com>
2025-11-03 11:26:02 +0200

    Restructure code and add text to home page

---
+++ src/js/partials.js
@@ -0,0 +1,37 @@
+fetch('partials/navbar.html')
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById('navbar').innerHTML = html;
+
+    const levelsMenu = document.getElementById('levelsMenu');
+    const levelsBtn = document.getElementById('levelsBtn');
+    const levelItems = document.querySelectorAll('.level-item');
+
+    levelsBtn.addEventListener('click', () => {
+        levelsMenu.classList.toggle('hidden');
+    });
+
+    window.addEventListener('click', (e) => {
+        if (!levelsBtn.contains(e.target) && !levelsMenu.contains(e.target)) {
+        levelsMenu.classList.add('hidden');
+        }
+    });
+
+    levelItems.forEach(link => {
+        const level = link.dataset.level;
+        const unlocked = localStorage.getItem(`level${level}_unlocked`) === 'true';
+        if (!unlocked && level !== "1") {
+        const lock = document.createElement('span');
+        lock.textContent = '🔒';
+        lock.classList.add('ml-2');
+        link.appendChild(lock);
+        link.classList.add('bg-gray-700', 'opacity-60', 'cursor-not-allowed', 'pointer-events-none');
+        }
+    });
+  });
+
+fetch('partials/footer.html')
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById('footer').innerHTML = html;
+  });
